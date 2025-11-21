3f7e34af98240ab59533b408fccb331444500938
TlDinhKhoi <luacrack4399@gmail.com>
2025-11-21 13:12:01 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5,6 +5,7 @@ local Versions = {
 	V1 = "https://github.com/TlDinhKhoi/Xeter/raw/refs/heads/main/Version/V1.lua",
 	V2 = "https://github.com/TlDinhKhoi/Xeter/raw/refs/heads/main/Version/V2.lua",
 	V3 = "https://github.com/TlDinhKhoi/Xeter/raw/refs/heads/main/Version/V3.lua",
+	V4 = "https://github.com/TlDinhKhoi/Xeter/raw/refs/heads/main/Version/V4.lua",
 }
 
 do
